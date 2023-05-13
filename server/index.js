1e2a952cc6731794395f1d9e5e2f131000f28a52
haylin <eileenza@protonmail.com>
2023-05-13 14:29:15 -0400

    fixing queries and index to call the functions

--- server/index.js
+++ server/index.js
@@ -30,58 +30,7 @@ app.get('/test', (req, res) => {
 //Should return the links stored on your Postgres database
  app.get('/links', db.getLinks) 
 
-//GET: /links/:id
-//Should retrieve a link by id
-app.get("/links/:id", async(req, res) => {
-    try {
-        const { id } = req.params;
-        const link = await pool.query("SELECT * FROM links WHERE ID = $1", [id]);
-        res.json(link.rows[0]);
 
-    } catch (error) {
-        console.error(error.message)
-    }
-})
-
-
-//POST: /links
-//Should create a new link
-app.post("/links", async (req, res) => {
-    try {
-        const { name, URL } = req.body;
-        const newLink = await pool.query("INSERT INTO links (name, URL) VALUES($1, $2) RETURNING *", [name, URL]);
-        res.json(newLink.rows[0])
-    } catch (error) {
-        console.log(error.message)
-    }
-})
-
-//PUT: /links/:id
-//Should update a link at a specific id
-app.put("/links/:id", async (req, res) => {
-    try {
-        const { id } = req.params;
-        const { name } = req.body;
-        const { URL } = req.body;
-        const updateLink = await pool.query("UPDATE links SET name = $1, URL = $2 WHERE ID = $3", [name, URL, id]);
-        res.json("link was updated!")
-
-    } catch (error) {
-        console.error(error.message)
-    }
-})
-//DELETE: /links/:id
-//Should delete a link at a specific id
-app.delete("/links/:id", async (req, res) => {
-    try {
-        const { id } = req.params;
-        const deleteLink = await pool.query("DELETE FROM links WHERE ID = $1", [id]);
-        res.json("Todo was deleted!")
-
-    } catch (error) {
-        console.error(error.message)
-    }
-})
 
 app.listen(PORT, (req, res) => {
     console.log(`App is running on port ${PORT}.`)
